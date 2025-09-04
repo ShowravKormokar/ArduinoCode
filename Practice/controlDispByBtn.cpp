7b4dea21299b592f00676c8752b0d565cc282df4
ShowravKormokar <showravkormokar354@gmail.com>
2025-09-04 21:57:07 +0600

    Show 0-9 on seven segment display by pressing btn, each press increase num and print

--- Practice/controlDispByBtn.cpp
+++ Practice/controlDispByBtn.cpp
@@ -1,7 +1,8 @@
-// 7-Segment Display with Arduino (Common Cathode)
-// Pins for segments: a, b, c, d, e, f, g
+// Show 0-9 on seven segment display by pressing btn, each press increase num and print
 
+int btnPin = 9;
 int segPins[] = {2, 3, 4, 5, 6, 7, 8}; // a–g connected to Arduino pins
+
 int digits[10][7] = {
     // a b c d e f g
     {1, 1, 1, 1, 1, 1, 0}, // 0
@@ -16,21 +17,37 @@ int digits[10][7] = {
     {1, 1, 1, 1, 0, 1, 1}  // 9
 };
 
+int btnState = 0;
+int lastBtnState = 0;
+int btnStatus = 0; // current digit
+
 void setup()
 {
+    pinMode(btnPin, INPUT_PULLUP); // button with pull-up
     for (int i = 0; i < 7; i++)
     {
         pinMode(segPins[i], OUTPUT);
     }
+    displayDigit(btnStatus); // show 0 at start
 }
 
 void loop()
 {
-    for (int num = 0; num < 10; num++)
+    btnState = digitalRead(btnPin);
+
+    // detect button press (LOW because of INPUT_PULLUP)
+    if (btnState == LOW && lastBtnState == HIGH)
     {
-        displayDigit(num);
-        delay(1000); // Hold each number for 1 second
+        btnStatus++;
+        if (btnStatus > 9)
+        {
+            btnStatus = 0;
+        }
+        displayDigit(btnStatus);
+        delay(200); // debounce delay
     }
+
+    lastBtnState = btnState;
 }
 
 void displayDigit(int num)
